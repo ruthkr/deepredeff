85b74e25213515d08a91deaa68dcf8dbfe060843
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2020-04-20 00:03:57 +0100

    Added encode_one_hot() function

---
+++ R/encode_one_hot.R
@@ -0,0 +1,30 @@
+#' TOne-hot Encoder
+#'
+#' @param sequence Sequence
+#' @param max_length Maximum sequence length of the used training data
+#'
+#' @return Encoded sequence
+#' @export
+encode_one_hot <- function(sequence, max_length = 4034) {
+  keys <- as.list(1:20)
+
+  names(keys) <- c(
+    "R", "K", "D", "E", "Q", "N", "H", "S", "T", "Y",
+    "C", "W", "A", "I", "L", "M", "F", "V", "P", "G"
+  )
+
+  sequence <- sequence %>%
+    toupper() %>%
+    strsplit("") %>%
+    unlist()
+
+  encoded_sequence <- matrix(0, nrow = max_length, ncol = length(keys))
+
+  count <- 1
+  for (letter in sequence) {
+    encoded_sequence[count, keys[[letter]]] <- 1
+    count <- count + 1
+  }
+
+  return(encoded_sequence)
+}
