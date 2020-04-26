6fbe67a62410fe6bb6c601038bbde1d806df1d91
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2020-04-26 17:20:29 +0100

    Added support for multiple models for some pathogens in load_model()

--- R/load_model.R
+++ R/load_model.R
@@ -1,12 +1,25 @@
 #' Load model in HDF5 format
 #'
-#' @param pathogen the name of pathogen, example: bacteria, fungi, and oomycete
+#' @param pathogen Name of pathogen
 #'
 #' @return all of the hyperparamaters and parameters of particula model from specific chosen pathogen
 #' @export
-load_model <- function(pathogen = "all") {
-  model_path <- system.file("extdata", "weights", paste0(pathogen, ".hdf5"), package = "effectorpred")
-  model <- keras::load_model_hdf5(model_path)
+load_model <- function(pathogen = c("all", "bacteria", "fungi", "oomycete")) {
+  pathogen <- match.arg(pathogen)
 
-  return(model)
+  model_paths <- Sys.glob(paste0(system.file("extdata", "weights", package = "effectorpred"), "/", pathogen, "*.hdf5"))
+
+  model_names <- model_paths %>%
+    stringr::str_split("/") %>%
+    unlist() %>%
+    grep(".hdf5", x = ., value = TRUE) %>%
+    stringr::str_remove_all(paste0(pathogen, "_|.hdf5"))
+
+  model_list <- model_paths %>%
+    purrr::map(
+      keras::load_model_hdf5
+    ) %>%
+    `names<-`(model_names)
+
+  return(model_list)
 }
