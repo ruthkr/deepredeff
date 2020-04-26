6f17c72762ceda39003dc46f0b496eb3455a6a6d
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2020-04-27 00:27:26 +0100

    Added package-level documentation

---
+++ R/deeffpred.R
@@ -0,0 +1,13 @@
+#' \code{deeffpred} package
+#'
+#' Effector protein predictor using Deep Learning models.
+#'
+#' See the README on
+#' \href{https://github.com/ruthkr/deeffpred/}{GitHub}
+#'
+#' @docType package
+#' @name deeffpred
+NULL
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if (getRversion() >= "2.15.1") utils::globalVariables(c("."))
