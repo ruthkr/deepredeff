dd7814ea826ff453439493ccf4f356c525634409
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2020-04-27 00:26:05 +0100

    Fixed system.file() path and added message to load_model() function

--- R/load_model.R
+++ R/load_model.R
@@ -7,19 +7,26 @@
 load_model <- function(pathogen = c("all", "bacteria", "fungi", "oomycete")) {
   pathogen <- match.arg(pathogen)
 
-  model_paths <- Sys.glob(paste0(system.file("extdata", "weights", package = "effectorpred"), "/", pathogen, "*.hdf5"))
+  model_paths <- Sys.glob(paste0(system.file("extdata", "weights", package = "deeffpred"), "/", pathogen, "*.hdf5"))
 
+  # Model names for list
   model_names <- model_paths %>%
     stringr::str_split("/") %>%
     unlist() %>%
     grep(".hdf5", x = ., value = TRUE) %>%
     stringr::str_remove_all(paste0(pathogen, "_|.hdf5"))
 
+  # Clear Keras session graph
+  keras::k_clear_session()
+
+  # Load models
   model_list <- model_paths %>%
     purrr::map(
       keras::load_model_hdf5
     ) %>%
     `names<-`(model_names)
+
+  message(paste0("Loaded models: ", paste(model_names, collapse = ", "), " for pathogen ", pathogen, "."))
 
   return(model_list)
 }
