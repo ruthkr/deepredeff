e8e751ce58b8a7b1b7f27eee088b4ee12c7c149f
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2020-04-26 22:18:51 +0100

    Added fasta_to_df() function

---
+++ R/fasta_to_df.R
@@ -0,0 +1,30 @@
+#' FASTA to dataframe
+#'
+#' @param fasta_path Path to FASTA file
+#'
+#' @return Data frame
+#' @export
+fasta_to_df <- function(fasta_path) {
+  data_list <- seqinr::read.fasta(fasta_path)
+
+  data <- data_list %>%
+    purrr::map(
+      .f = function(x) {
+        data.frame(
+          c(
+            x %>%
+              attributes() %>%
+              unlist(),
+            x %>%
+              paste0(collapse = "") %>%
+              toupper() %>%
+              `names<-`("sequence")
+          ) %>%
+            dplyr::bind_rows()
+        )
+      }
+    ) %>%
+    purrr::reduce(dplyr::bind_rows)
+
+  return(data)
+}
