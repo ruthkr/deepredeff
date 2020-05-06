5916556511e21729275eb409889f3fba8f0f0423
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2020-05-06 14:32:36 +0100

    updated encoding methods

--- R/encoding_methods.R
+++ R/encoding_methods.R
@@ -13,12 +13,21 @@ encode_one_hot <- function(sequence, max_length = 4034) {
     "C", "W", "A", "I", "L", "M", "F", "V", "P", "G"
   )
 
+  # Split the amino acid letter in the sequence
   sequence <- sequence %>%
+    # Cut or pad sequence
+    substr(1, max_length) %>%
+    sprintf(paste0("%-", max_length, "s"), .) %>%
+    # Capitalize and split
     toupper() %>%
     strsplit("") %>%
     unlist()
 
-  encoded_sequence <- matrix(0, nrow = max_length, ncol = length(keys))
+  encoded_sequence <- matrix(
+    0,
+    nrow = max_length,
+    ncol = length(keys)
+  )
 
   count <- 1
   for (letter in sequence) {
@@ -26,8 +35,8 @@ encode_one_hot <- function(sequence, max_length = 4034) {
     count <- count + 1
   }
 
-  encoded_sequence <- encoded_sequence %>%
-    array(dim = c(1, max_length, length(keys)))
+  # encoded_sequence <- encoded_sequence %>%
+  #   array(dim = c(1, max_length, length(keys)))
 
   return(encoded_sequence)
 }
@@ -41,10 +50,10 @@ encode_one_hot <- function(sequence, max_length = 4034) {
 #' @export
 encode_integer <- function(sequence, max_length = 4034) {
   # Define the list of the letters
-  keys <- as.list(1:26)
+  keys <- as.list(0:26)
 
   names(keys) <- c(
-    "A", "C", "D", "E", "F", "G", "H", "I",
+    " ", "A", "C", "D", "E", "F", "G", "H", "I",
     "K", "L", "M", "N", "P", "Q", "R", "S",
     "T", "V", "W", "Y", "X", "B", "U", "J",
     "Z", "O"
@@ -52,16 +61,24 @@ encode_integer <- function(sequence, max_length = 4034) {
 
   # Split the amino acid letter in the sequence
   sequence <- sequence %>%
+    # Cut or pad sequence
+    substr(1, max_length) %>%
+    sprintf(paste0("%-", max_length, "s"), .) %>%
+    # Capitalize and split
     toupper() %>%
     strsplit("") %>%
     unlist()
 
   # Initialize the array of zeros
-  encoded_sequence <- array(0, c(1, max_length))
+  encoded_sequence <- matrix(
+    0,
+    nrow = 1,
+    ncol = max_length
+  )
 
   index <- 1
   for (amino_acid in sequence) {
-    encoded_sequence[, index] <- keys[[amino_acid]]
+    encoded_sequence[1, index] <- keys[[amino_acid]]
     index <- index + 1
   }
 
