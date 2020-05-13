d183a8750970ebcacab7a9ad22dcd8199aacd8a7
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2020-05-13 23:29:04 +0100

    added predict_mapper as a prediction helper function

---
+++ R/prediction_helper.R
@@ -0,0 +1,45 @@
+#' Prediction mapper helper
+#'
+#' @param sequence_list List of sequences.
+#' @param model_list List of models.
+#'
+#' @return List of predictions.
+prediction_mapper <- function(sequence_list, model_list) {
+  pred_list <- purrr::map(
+    .x = model_list,
+    .f = function(model) {
+      # Max length
+      max_length <- unlist(model$layers[[1]]$input_shape)[[1]]
+
+      # Select encoding method
+      if (any(grep("embedding", as.character(model$layers[[1]])))) {
+        encode_method <- encode_integer
+        array_dim <- c(length(sequence_list), max_length)
+      } else {
+        encode_method <- encode_one_hot
+        array_dim <- c(length(sequence_list), max_length, 20)
+      }
+
+      sequence_array <- sequence_list %>%
+        purrr::map(
+          .f = function(x) {
+            sequence <- encode_method(x, max_length)
+            return(sequence)
+          }
+        ) %>%
+        do.call(rbind, .) %>%
+        array(dim = array_dim)
+
+      # Make prediction
+      pred <- stats::predict(model, sequence_array) # %>%
+      # as.numeric()
+
+      return(pred)
+    }
+  )
+
+  return(pred_list)
+}
+
+
+
