b2305ad18627760ae6f4f44d1ebc36a77c1a0d18
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2020-05-13 23:00:18 +0100

    updated NAMESPACE from the S3 object

--- R/prediction.R
+++ R/prediction.R
@@ -1,93 +1,28 @@
-#' Prediction mapper helper
-#'
-#' @param sequence_list List of sequences.
-#' @param model_list List of models.
-#'
-#' @return List of predictions.
-prediction_mapper <- function(sequence_list, model_list) {
-  pred_list <- purrr::map(
-    .x = model_list,
-    .f = function(model) {
-      # Max length
-      max_length <- unlist(model$layers[[1]]$input_shape)[[1]]
-
-      # Select encoding method
-      if (any(grep("embedding", as.character(model$layers[[1]])))) {
-        encode_method <- encode_integer
-        array_dim <- c(length(sequence_list), max_length)
-      } else {
-        encode_method <- encode_one_hot
-        array_dim <- c(length(sequence_list), max_length, 20)
-      }
-
-      sequence_array <- sequence_list %>%
-        purrr::map(
-          .f = function(x) {
-            sequence <- encode_method(x, max_length)
-            return(sequence)
-          }
-        ) %>%
-        do.call(rbind, .) %>%
-        array(dim = array_dim)
-
-      # Make prediction
-      pred <- stats::predict(model, sequence_array) # %>%
-      # as.numeric()
-
-      return(pred)
-    }
-  )
-
-  return(pred_list)
-}
-
-
 #' Predict effector
 #'
 #' @param input FASTA path or dataframe that contains input sequences.
 #' @param model Selected model.
-#' @param col_names Optional column names for dataframe. One of them must be named 'sequence'.
 #'
 #' @return Dataframe with sequences and effector predictions.
 #' @export
-#'
-#' @importFrom rlang .data
-predict_effector <- function(input, model = "all", col_names) {
-  stopifnot(model %in% c("all", "bacteria", "fungi", "oomycete"))
+predict_effector <- function(input, model) {
+  UseMethod("predict_effector", input)
+}
 
-  # Check the class of the input
-  class_input <- class(input) %>% .[1]
-
-  if (class_input == "character") {
-    sequence_df <- fasta_to_df(input) %>%
-      dplyr::select(.data$name, .data$sequence)
-  } else if (class_input %in% c("AAString", "AAStringSet")) {
-    sequence_df <- aas_to_df(input) %>%
-      dplyr::rename(sequence = seq)
-  } else {
-    sequence_df <- input %>%
-      dplyr::select({{ col_names }})
-  }
+#' @rdname predict_effector
+#' @export
+predict_effector.character <- function(input, model) {
+  sequence_df <- fasta_to_df(input) %>%
+    dplyr::select(.data$name, .data$sequence)
 
   # Make list of sequences
   sequence_list <- sequence_df %>%
     dplyr::pull(sequence) %>%
     as.list()
 
-
-  # Select ensemble method
-  if (model == "bacteria") {
-    ensemble_method <- ensemble_weighted
-    weights <- bacteria_weights
-  } else if (model == "all") {
-    ensemble_method <- ensemble_voting
-    weights <- bacteria_weights
-  } else {
-    ensemble_method <- function(x, y) {
-      return(x[[1]])
-    }
-    weights <- NULL
-  }
+  # Select ensemble method and weights
+  ensemble_method <- get_ensemble_method(model)[[1]]
+  weights <- get_ensemble_method(model)[[2]]
 
   # Load model
   model_list <- load_model(model)
@@ -102,4 +37,70 @@ predict_effector <- function(input, model = "all", col_names) {
   )
 
   return(preds)
+}
+
+
+#' @rdname predict_effector
+#' @export
+predict_effector.DataFrame <- function(input, model) {
+  # Make list of sequences
+  sequence_list <- input %>%
+    dplyr::pull(sequence) %>%
+    as.list()
+
+  # Select ensemble method and weights
+  ensemble_method <- get_ensemble_method(model)[[1]]
+  weights <- get_ensemble_method(model)[[2]]
+
+  # Load model
+  model_list <- load_model(model)
+  message("Loaded models successfully!")
+
+  # Make predictions
+  pred_list <- prediction_mapper(sequence_list, model_list)
+
+  preds <- dplyr::bind_cols(
+    input,
+    prob = unlist(ensemble_method(pred_list, weights))
+  )
+
+  return(preds)
+}
+
+
+#' @rdname predict_effector
+#' @export
+predict_effector.AAStringset <- function(input, model) {
+  # Make list of sequences
+  sequence_df <- aas_to_df(input) %>%
+    dplyr::rename(sequence = seq)
+
+  sequence_list <- sequence_df %>%
+    dplyr::pull(sequence) %>%
+    as.list()
+
+  # Select ensemble method and weights
+  ensemble_method <- get_ensemble_method(model)[[1]]
+  weights <- get_ensemble_method(model)[[2]]
+
+  # Load model
+  model_list <- load_model(model)
+  message("Loaded models successfully!")
+
+  # Make predictions
+  pred_list <- prediction_mapper(sequence_list, model_list)
+
+  preds <- dplyr::bind_cols(
+    sequence_df,
+    prob = unlist(ensemble_method(pred_list, weights))
+  )
+
+  return(preds)
+}
+
+
+#' @rdname predict_effector
+#' @export
+predict_effector.default <- function(input, model) {
+  warning(paste("deepredeff does not know how to handle input", class(input), "and currecntly can only be used on fasta, AAStringset, and dataframe input"))
 }
