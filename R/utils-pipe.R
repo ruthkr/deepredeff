366927df22490cb252c35be5d3c9f308d9d4e784
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2020-05-15 21:45:29 +0100

    added `%<-%` from `zeallot` to assign value to names

--- R/utils-pipe.R
+++ R/utils-pipe.R
@@ -9,3 +9,15 @@
 #' @importFrom magrittr %>%
 #' @usage lhs \%>\% rhs
 NULL
+
+#' Assign values to names
+#'
+#' See \code{\link[zeallot]{\%<-\%}} for more details.
+#'
+#' @name %<-%
+#' @rdname multi-assign
+#' @keywords internal
+#' @export
+#' @import zeallot
+#' @usage x \%<-\% value
+NULL
