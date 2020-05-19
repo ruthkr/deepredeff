0770329ce78118907caf5a28ecce8037b99385ae
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2020-05-19 23:46:30 +0100

    Added test for transform_input functions

---
+++ tests/testthat.R
@@ -0,0 +1,6 @@
+library(testthat)
+# reticulate::use_condaenv("tensorflow", conda = "/usr/local/Caskroom/miniconda/base/bin/conda")
+# library(keras)
+library(deepredeff)
+
+test_check("deepredeff")
---
+++ tests/testthat/test-transform_input.R
@@ -0,0 +1,43 @@
+test_that("FASTA conversion to data frame works", {
+  expect_equal(
+    system.file("extdata/example/fungi_sample.fasta", package = "deepredeff") %>%
+      fasta_to_df() %>%
+      class(),
+    "data.frame"
+  )
+})
+
+test_that("AAStringset conversion to data frame works", {
+  expect_equal(
+    system.file("extdata/example/fungi_sample.fasta", package = "deepredeff") %>%
+      Biostrings::readAAStringSet() %>%
+      aasset_to_df() %>%
+      class(),
+    "data.frame"
+  )
+})
+
+test_that("AAString conversion to data frame works", {
+  expect_equal(
+    system.file("extdata/example/fungi_sample.fasta", package = "deepredeff") %>%
+      fasta_to_df() %>%
+      dplyr::slice(1) %>%
+      dplyr::pull(sequence) %>%
+      Biostrings::AAString() %>%
+      aas_to_df() %>%
+      class(),
+    "data.frame"
+  )
+})
+
+test_that("FASTA and AAStringset conversion to data frame works", {
+  expect_equal(
+    system.file("extdata/example/fungi_sample.fasta", package = "deepredeff") %>%
+      fasta_to_df() %>%
+      dplyr::pull(sequence),
+    system.file("extdata/example/fungi_sample.fasta", package = "deepredeff") %>%
+      Biostrings::readAAStringSet() %>%
+      aasset_to_df() %>%
+      dplyr::pull(seq)
+  )
+})
