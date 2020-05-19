f6acd74aa8e6448b120fe34bc72aa3df33fbdd71
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2020-05-19 23:45:13 +0100

    Added autoplot generic for tbl_deepredeff S3 object

--- R/tbl_deepredeff.R
+++ R/tbl_deepredeff.R
@@ -6,6 +6,12 @@ new_tbl_deepredeff <- function(x, description = names(x)) {
   structure(x, class = c("tbl_deepredeff", "data.frame"), description = description)
 }
 
+#' @export
+`[.tbl_deepredeff` <- function(x, i, ...) {
+  new_x <- NextMethod("[")
+  new_tbl_deepredeff(new_x)
+}
+
 #' Get the summary of the prediction results
 #'
 #' @param object Results of prediction from deepredeff::predict_effector().
@@ -35,8 +41,68 @@ summary.tbl_deepredeff <- function(object, cutoff = 0.5, ...) {
 }
 
 
+
+
+
+
+#' Plot the results of prediction
+#'
+#' @param object Results of prediction from deepredeff::predict_effector().
+#' @param cutoff Cutoff value to determine the prediction class (1 for effector and 0 for non-effector). The default value is 0.5.
+#' @param type Type plot to generate. Type class will generate histogram of each prediction class, while prob will show each sequence prediction value and the cutoff value. Default type is class.
+#' @param ... Additional arguments ignored.
+#'
+#' @return plot depends on the chosen type
 #' @export
-`[.tbl_deepredeff` <- function(x, i, ...) {
-  new_x <- NextMethod("[")
-  new_tbl_deepredeff(new_x)
+#' @importFrom rlang .data
+#' @importFrom ggplot2 autoplot
+autoplot.tbl_deepredeff <- function(object, cutoff = 0.5, type = "class", ...) {
+
+  object <- object %>%
+    dplyr::mutate(
+      pred_class = dplyr::case_when(
+        .data$prob >= cutoff ~ 1,
+        .data$prob < cutoff ~ 0
+      )
+    )
+
+  switch(
+    type,
+    "class" = gg_class(object, cutoff),
+    "prob" = gg_prob(object, cutoff)
+  )
+}
+
+#' @importFrom rlang .data
+#' @importFrom ggplot2 autoplot
+gg_class <- function(object, cutoff) {
+  `%+%` <- ggplot2::`%+%`
+
+  object %>%
+    ggplot2::ggplot() %+%
+    ggplot2::aes(.data$pred_class) %+%
+    ggplot2::geom_bar() %+%
+    ggplot2::labs(x = "Prediction class")
+}
+
+#' @importFrom rlang .data
+#' @importFrom ggplot2 autoplot
+gg_prob <- function(object, cutoff) {
+  `%+%` <- ggplot2::`%+%`
+
+  object %>%
+    ggplot2::ggplot() %+%
+    ggplot2::aes(
+      x = .data$name,
+      y = .data$prob,
+      ymin = 0,
+      ymax = .data$prob
+    ) %+%
+    ggplot2::geom_point() %+%
+    ggplot2::geom_hline(
+      ggplot2::aes(yintercept = cutoff),
+      colour = "blueviolet",
+      linetype = "dashed"
+    ) %+%
+    ggplot2::geom_linerange()
 }
