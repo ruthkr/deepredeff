f524b299735af93d01bf9de649d65b5e486abc62
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2020-06-12 10:36:59 +0100

    Skip tests that require TF/Keras following  reticulate's vignette (https://rstudio.github.io/reticulate/articles/package.html#checking-and-testing-on-cran-1)

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,4 @@
 library(testthat)
-# reticulate::use_condaenv("tensorflow", conda = "/usr/local/Caskroom/miniconda/base/bin/conda")
-# library(keras)
 library(deepredeff)
 
 test_check("deepredeff")
--- tests/testthat/test-prediction.R
+++ tests/testthat/test-prediction.R
@@ -1,4 +1,15 @@
+# helper function to skip tests if we don't have the 'tensorflow' module
+skip_if_no_tf <- function() {
+  have_tf <- reticulate::py_module_available("tensorflow")
+  have_keras <- reticulate::py_module_available("keras")
+
+  if (!as.logical(have_tf * have_keras)) {
+    skip("TensorFlow and Keras not available for testing")
+  }
+}
+
 test_that("Prediction function works", {
+  skip_if_no_tf()
   expect_equal(
     deepredeff::predict_effector(
       input = system.file("extdata/example/fungi_sample.fasta", package = "deepredeff"),
