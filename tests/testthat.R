ba31a06d8480ada088e979338079a76e41dcb245
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2020-06-18 14:51:42 +0100

    Add py_discover_config() to travis CI to help track down test issue and specified conda path when installing TF 2.0

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,5 @@
 library(testthat)
-reticulate::use_condaenv('r-reticulate')
+# reticulate::use_condaenv('r-reticulate')
 library(deepredeff)
 
 test_check("deepredeff", reporter = c('progress', 'fail'))
