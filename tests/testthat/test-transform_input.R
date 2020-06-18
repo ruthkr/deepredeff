72d79798d2b21ddb1024aa6015c53083db20ba05
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2020-06-18 13:40:16 +0100

    Check sequence pulling individually for each input format

--- tests/testthat/test-transform_input.R
+++ tests/testthat/test-transform_input.R
@@ -5,6 +5,13 @@ test_that("FASTA conversion to data frame works", {
       class(),
     "data.frame"
   )
+  expect_equal(
+    system.file("extdata/example/fungi_sample.fasta", package = "deepredeff") %>%
+      fasta_to_df() %>%
+      dplyr::pull(sequence) %>%
+      class(),
+    "character"
+  )
 })
 
 test_that("AAStringset conversion to data frame works", {
@@ -14,6 +21,14 @@ test_that("AAStringset conversion to data frame works", {
       aasset_to_df() %>%
       class(),
     "data.frame"
+  )
+  expect_equal(
+    system.file("extdata/example/fungi_sample.fasta", package = "deepredeff") %>%
+      Biostrings::readAAStringSet() %>%
+      aasset_to_df() %>%
+      dplyr::pull(seq) %>%
+      class(),
+    "character"
   )
 })
 
@@ -28,18 +43,27 @@ test_that("AAString conversion to data frame works", {
       class(),
     "data.frame"
   )
-})
-
-test_that("FASTA and AAStringset conversion to data frame works", {
   expect_equal(
     system.file("extdata/example/fungi_sample.fasta", package = "deepredeff") %>%
       fasta_to_df() %>%
-      .[["sequence"]],
-    system.file("extdata/example/fungi_sample.fasta", package = "deepredeff") %>%
-      Biostrings::readAAStringSet() %>%
-      aasset_to_df() %>%
-      .[["seq"]]
+      dplyr::slice(1) %>%
+      dplyr::pull(sequence) %>%
+      Biostrings::AAString() %>%
+      aas_to_df() %>%
+      dplyr::pull(seq) %>%
+      class(),
+    "character"
   )
 })
 
-
+# test_that("FASTA and AAStringset conversion to data frame works", {
+#   expect_equal(
+#     system.file("extdata/example/fungi_sample.fasta", package = "deepredeff") %>%
+#       fasta_to_df() %>%
+#       .[["sequence"]],
+#     system.file("extdata/example/fungi_sample.fasta", package = "deepredeff") %>%
+#       Biostrings::readAAStringSet() %>%
+#       aasset_to_df() %>%
+#       .[["seq"]]
+#   )
+# })
