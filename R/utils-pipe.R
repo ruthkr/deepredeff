3af26fca48d02784606a21923bcb1942e5729847
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2020-06-22 11:58:53 +0100

    updated utils-pipe

--- R/utils-pipe.R
+++ R/utils-pipe.R
@@ -9,21 +9,3 @@
 #' @importFrom magrittr %>%
 #' @usage lhs \%>\% rhs
 NULL
-
-#' As operator
-#'
-#' @importFrom reticulate %as%
-#' @export
-reticulate::`%as%`
-
-#' Assign values to names
-#'
-#' See \code{\link[zeallot]{\%<-\%}} for more details.
-#'
-#' @name %<-%
-#' @rdname multi-assign
-#' @keywords internal
-#' @export
-#' @import zeallot
-#' @usage x \%<-\% value
-NULL
