64de576b8bd86f63db763db392e5343e229ca416
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2020-07-01 16:40:28 +0100

    added some attributes in predict effector,  update predict_effector for character so that it can handle vector input

--- R/prediction.R
+++ R/prediction.R
@@ -1,20 +1,29 @@
 #' Predict effector
 #'
-#' @param input Input data that contains amino acid sequence(s). It can be in fasta format, strings, AAString, AAStringset, and dataframe.
-#' @param model Model to use in predicting the input sequence. Available models are bacteria, fungi, oomycete, and all.
+#' `predict_effector` is used to predict effector protein given amino acid protein sequences.
 #'
-#' @return Deepredeff objects that contains sequences and effector predictions.
+#' @param input Input data that contains amino acid sequence(s). It can be in fasta format, strings, AAString, AAStringset, and dataframe.
+#' @param taxon Taxon group of input data. The default is is "all". Available taxons are bacteria, fungi, oomycete, and all.
+#'
+#' @return `predict_effector` returns an object of class "tbl_deepredeff" or for multiple responses of class c("tbl_deepredeff", "data.frame").
+#'
+#' An object of class "tbl_deepredeff" is a data frame containing at least the following components:
+#'
+#' \item{`sequence`}{the sequence(s) from the input data.}
+#' \item{`s_score`}{score obtained from sigmoid function showing how likely the sequences to be an effector.}
+#' \item{`prediction`}{class prediction for each sequence, obtained from s_score. If the value of `s_score` ≥ 0.5, it will be clasified as an effector. Otherwise, it will be claasified as a non-effector.}
+#'
 #' @export
 #'
 #' @importFrom rlang .data
-predict_effector <- function(input, model) {
+predict_effector <- function(input, taxon) {
   UseMethod("predict_effector", input)
 }
 
 #' @rdname predict_effector
 #' @export
-predict_effector.character <- function(input, model) {
-  if (tolower(tools::file_ext(input)) %in% c("fasta", "fna", "ffn", "faa", "frn", "txt", "csv")) {
+predict_effector.character <- function(input, taxon) {
+  if (all(tolower(tools::file_ext(input)) %in% c("fasta", "fna", "ffn", "faa", "frn", "txt", "csv"))) {
     sequence_df <- fasta_to_df(input)
   } else {
     stopifnot(grepl("^[A-Za-z]+$", input, perl = T))
@@ -31,20 +40,38 @@ predict_effector.character <- function(input, model) {
     as.list()
 
   # Select ensemble method and weights
-  ensemble_method <- get_ensemble_method(model)[[1]]
-  weights <- get_ensemble_method(model)[[2]]
+  ensemble_method <- get_ensemble_method(taxon)[[1]]
+  weights <- get_ensemble_method(taxon)[[2]]
 
   # Load model
-  model_list <- load_model(model)
+  model_list <- load_model(taxon)
   message("Loaded models successfully!")
+
+  model_names <- names(model_list)
+
+  # Model names for list
+  if (taxon == "bacteria"){
+    model_names <- "ensemble_weighted"
+  } else if (taxon == "all"){
+    model_names <- "ensemble_voting"
+  }
+
+  message(paste0("Model used ", "for taxon ", taxon, ": ", paste(model_names, collapse = ", "), "."))
+
 
   # Make predictions
   pred_list <- prediction_mapper(sequence_list, model_list)
 
   preds <- dplyr::bind_cols(
     sequence_df,
-    prob = unlist(ensemble_method(pred_list, weights))
+    s_score = unlist(ensemble_method(pred_list, weights))
   )
+
+  preds <- preds %>%
+    dplyr::mutate(prediction = ifelse(s_score >= 0.5, "effector", "non-effector"))
+
+  attr(preds, "model_names") <- model_names
+  attr(preds, "taxon") <- taxon
 
   return(new_tbl_deepredeff(preds))
 }
@@ -52,27 +79,44 @@ predict_effector.character <- function(input, model) {
 
 #' @rdname predict_effector
 #' @export
-predict_effector.data.frame <- function(input, model) {
+predict_effector.data.frame <- function(input, taxon) {
   # Make list of sequences
   sequence_list <- input %>%
     dplyr::pull(sequence) %>%
     as.list()
 
   # Select ensemble method and weights
-  ensemble_method <- get_ensemble_method(model)[[1]]
-  weights <- get_ensemble_method(model)[[2]]
+  ensemble_method <- get_ensemble_method(taxon)[[1]]
+  weights <- get_ensemble_method(taxon)[[2]]
 
   # Load model
-  model_list <- load_model(model)
+  model_list <- load_model(taxon)
   message("Loaded models successfully!")
+
+  model_names <- names(model_list)
+
+  # Model names for list
+  if (taxon == "bacteria"){
+    model_names <- "ensemble_weighted"
+  } else if (taxon == "all"){
+    model_names <- "ensemble_voting"
+  }
+
+  message(paste0("Model used ", "for taxon ", taxon, ": ", paste(model_names, collapse = ", "), "."))
 
   # Make predictions
   pred_list <- prediction_mapper(sequence_list, model_list)
 
   preds <- dplyr::bind_cols(
     input,
-    prob = unlist(ensemble_method(pred_list, weights))
+    s_score = unlist(ensemble_method(pred_list, weights))
   )
+
+  preds <- preds %>%
+  dplyr::mutate(prediction = ifelse(s_score >= 0.5, "effector", "non-effector"))
+
+  attr(preds, "model_names") <- model_names
+  attr(preds, "taxon") <- taxon
 
   return(new_tbl_deepredeff(preds))
 }
@@ -80,7 +124,7 @@ predict_effector.data.frame <- function(input, model) {
 
 #' @rdname predict_effector
 #' @export
-predict_effector.AAStringSet <- function(input, model) {
+predict_effector.AAStringSet <- function(input, taxon) {
   # Make list of sequences
   sequence_df <- aasset_to_df(input) %>%
     dplyr::rename(sequence = seq)
@@ -90,20 +134,37 @@ predict_effector.AAStringSet <- function(input, model) {
     as.list()
 
   # Select ensemble method and weights
-  ensemble_method <- get_ensemble_method(model)[[1]]
-  weights <- get_ensemble_method(model)[[2]]
+  ensemble_method <- get_ensemble_method(taxon)[[1]]
+  weights <- get_ensemble_method(taxon)[[2]]
 
   # Load model
-  model_list <- load_model(model)
+  model_list <- load_model(taxon)
   message("Loaded models successfully!")
+
+  model_names <- names(model_list)
+
+  # Model names for list
+  if (taxon == "bacteria"){
+    model_names <- "ensemble_weighted"
+  } else if (taxon == "all"){
+    model_names <- "ensemble_voting"
+  }
+
+  message(paste0("Model used ", "for taxon ", taxon, ": ", paste(model_names, collapse = ", "), "."))
 
   # Make predictions
   pred_list <- prediction_mapper(sequence_list, model_list)
 
   preds <- dplyr::bind_cols(
     sequence_df,
-    prob = unlist(ensemble_method(pred_list, weights))
+    s_score = unlist(ensemble_method(pred_list, weights))
   )
+
+  preds <- preds %>%
+    dplyr::mutate(prediction = ifelse(s_score >= 0.5, "effector", "non-effector"))
+
+  attr(preds, "model_names") <- model_names
+  attr(preds, "taxon") <- taxon
 
   return(new_tbl_deepredeff(preds))
 }
@@ -111,7 +172,7 @@ predict_effector.AAStringSet <- function(input, model) {
 
 #' @rdname predict_effector
 #' @export
-predict_effector.AAString <- function(input, model) {
+predict_effector.AAString <- function(input, taxon) {
   # Make list of sequences
   sequence_df <- aas_to_df(input) %>%
     dplyr::rename(sequence = seq)
@@ -121,20 +182,37 @@ predict_effector.AAString <- function(input, model) {
     as.list()
 
   # Select ensemble method and weights
-  ensemble_method <- get_ensemble_method(model)[[1]]
-  weights <- get_ensemble_method(model)[[2]]
+  ensemble_method <- get_ensemble_method(taxon)[[1]]
+  weights <- get_ensemble_method(taxon)[[2]]
 
   # Load model
-  model_list <- load_model(model)
+  model_list <- load_model(taxon)
   message("Loaded models successfully!")
+
+  model_names <- names(model_list)
+
+  # Model names for list
+  if (taxon == "bacteria"){
+    model_names <- "ensemble_weighted"
+  } else if (taxon == "all"){
+    model_names <- "ensemble_voting"
+  }
+
+  message(paste0("Model used ", "for taxon ", taxon, ": ", paste(model_names, collapse = ", "), "."))
 
   # Make predictions
   pred_list <- prediction_mapper(sequence_list, model_list)
 
   preds <- dplyr::bind_cols(
     sequence_df,
-    prob = unlist(ensemble_method(pred_list, weights))
+    s_score = unlist(ensemble_method(pred_list, weights))
   )
+
+  preds <- preds %>%
+    dplyr::mutate(prediction = ifelse(s_score >= 0.5, "effector", "non-effector"))
+
+  attr(preds, "model_names") <- model_names
+  attr(preds, "taxon") <- taxon
 
   return(new_tbl_deepredeff(preds))
 }
@@ -142,6 +220,6 @@ predict_effector.AAString <- function(input, model) {
 
 #' @rdname predict_effector
 #' @export
-predict_effector.default <- function(input, model) {
+predict_effector.default <- function(input, taxon) {
   warning(paste("deepredeff does not know how to handle input", class(input), "and currently can only be used on fasta, AAStringset, and dataframe input"))
 }
