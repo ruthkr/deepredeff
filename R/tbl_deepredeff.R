37928d203b66d76bbb07ebb6366f41cbbf330f65
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2020-07-02 22:41:54 +0100

    New plot and summary generic methods for tbl_deepredeff S3 class

--- R/tbl_deepredeff.R
+++ R/tbl_deepredeff.R
@@ -15,63 +15,78 @@ new_tbl_deepredeff <- function(x, description = names(x)) {
 #' Get the summary of the prediction results
 #'
 #' @param object Results of prediction from deepredeff::predict_effector().
-#' @param cutoff Cutoff value to determine the prediction class (1 for effector and 0 for non-effector). The default value is 0.5.
 #' @param ... Additional arguments ignored.
 #'
 #' @export
 #' @importFrom rlang .data
-summary.tbl_deepredeff <- function(object, cutoff = 0.5, ...) {
-  object <- object %>%
-    dplyr::mutate(
-      pred_class = dplyr::case_when(
-        .data$prob >= cutoff ~ "effector",
-        .data$prob < cutoff ~ "non-effector"
-      )
+summary.tbl_deepredeff <- function(object, ...) {
+
+  table_summary <- table(factor(object$prediction, levels = c("effector", "non-effector")))
+
+  res <- list(
+    total_seq_input = nrow(object),
+    model_names = attr(object, "model_names"),
+    taxon = attr(object, "taxon"),
+    num_effector = table_summary[["effector"]],
+    num_non_effector = table_summary[["non-effector"]]
+  )
+
+  class(res) <- "summary.tbl_deepredeff"
+
+  return(res)
+}
+
+#' @export
+print.summary.tbl_deepredeff <- function(x, ...) {
+  cat(
+    paste0(
+      "Total sequences in input data: ", x$total_seq_input,
+      "\n---\n",
+      "Taxon chosen: ", x$taxon,
+      "\n",
+      "Model type used: ", x$model_names,
+      "\n\n",
+      "Total sequences predicted as effector: ", x$num_effector,
+      "\n",
+      "Total sequences predicted as non-effector: ", x$num_non_effector
     )
-
-  count_per_class <- object %>%
-    dplyr::group_by(.data$pred_class) %>%
-    dplyr::summarise(count = dplyr::n())
-
-  proportion_per_class <- object %>%
-    dplyr::group_by(.data$pred_class) %>%
-    dplyr::summarise(proportion = dplyr::n() / nrow(object))
-
-  df_summary <- dplyr::left_join(count_per_class, proportion_per_class, by = "pred_class")
-
-  return(df_summary)
+  )
 }
 
 
 #' Plot the results of prediction
 #'
-#' @param object Results of prediction from deepredeff::predict_effector().
-#' @param cutoff Cutoff value to determine the prediction class (1 for effector and 0 for non-effector). The default value is 0.5.
-#' @param ... Additional arguments ignored.
+#' @param x tbl_deepredeff object
+#' @param ... additional arguments ignored.
 #'
-#' @return Plot depending on the chosen type.
+#' @return class distribution plot
 #' @export
 #' @importFrom rlang .data
-#' @importFrom ggplot2 autoplot
-autoplot.tbl_deepredeff <- function(object, cutoff = 0.5, ...) {
+plot.tbl_deepredeff <- function(x, ...) {
   `%+%` <- ggplot2::`%+%`
-  object <- object %>%
-    dplyr::mutate(
-      class = factor(
-        ifelse(.data$prob >= cutoff, 1, 0),
-        levels = c(1, 0),
-        labels = c("effector", "non-effector")
-      )
-    )
 
-  plot <- object %>%
+  # Set message either group has fewer than two data points
+  cond_effector <- (table(x$prediction)[["effector"]] < 2)
+  cond_non_effector <- (table(x$prediction)[["non-effector"]] < 2)
+
+  cond <- c(cond_effector, cond_non_effector)
+  names(cond) <- c("effector", "non-effector")
+
+  if (cond_effector | cond_non_effector) {
+    message(paste0("Plot can not be generated for group ", paste(names(cond[which(cond)]), collapse = " and group "), ". Each group needs to have at least two data points."))
+  }
+
+  plot <- x %>%
     ggplot2::ggplot() +
     ggplot2::aes(
-      x = .data$prob,
-      fill = .data$class
+      x = .data$s_score,
+      fill = .data$prediction
     ) +
     ggplot2::geom_density(alpha = 0.5) +
-    ggplot2::scale_x_continuous(breaks = seq(0, 1, 0.1))
+    ggplot2::scale_x_continuous(breaks = seq(0, 1, 0.1)) +
+    ggplot2::labs(x = "S-score", y = "Density", fill = "Prediction") +
+    ggthemes::scale_fill_ptol()
+
 
   return(plot)
 }
