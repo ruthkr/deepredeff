7f13e76804fb069f3b8b3327ab7e4ad1ecdb2c92
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2020-07-07 13:34:26 +0100

    updated the test with ensemble_voting()

--- tests/testthat/test-ensemble_methods.R
+++ tests/testthat/test-ensemble_methods.R
@@ -20,35 +20,10 @@ test_that("Ensemble weighted works", {
   )
 })
 
-test_that("Ensemble voting works", {
-  pred_list_length <- 10L
-  ens_results <- deepredeff::ensemble_voting(
-    pred_list = list(
-      cnn_gru = as.list(runif(pred_list_length)),
-      cnn_lstm = as.list(runif(pred_list_length)),
-      gru_emb = as.list(runif(pred_list_length)),
-      lstm_emb = as.list(runif(pred_list_length))
-    ),
-    weights = NULL
-  )
-
-  expect_equal(
-    length(ens_results),
-    pred_list_length
-  )
-
-  expect_true(
-    all(ens_results == 0 | ens_results == 1)
-  )
-})
-
 
 test_that("Ensemble method selection works", {
   expect_equal(
     deepredeff::get_ensemble_method("bacteria") %>% class(),
     "list"
-  )
-  expect_true(
-    deepredeff::get_ensemble_method("all")[[2]] %>% is.null()
   )
 })
